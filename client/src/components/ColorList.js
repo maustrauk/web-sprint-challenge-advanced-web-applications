dd3063a6b39f92dce99f34cfcf8bd9591e147b48
Iaroslav Mokroguz <maustrauk@gmail.com>
2020-12-18 14:04:52 -0500

    Polish editColor form

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import { axiosWithAuth } from './../utils/axiosWithAuth';
 
+import EditColorForm from './../forms/EditColorForm';
+
 const initialColor = {
   color: "",
   code: { hex: "" }
@@ -68,34 +70,7 @@ const ColorList = props => {
         ))}
       </ul>
       {editing && (
-        <form onSubmit={saveEdit}>
-          <legend>edit color</legend>
-          <label>
-            color name:
-            <input
-              onChange={e =>
-                setColorToEdit({ ...colorToEdit, color: e.target.value })
-              }
-              value={colorToEdit.color}
-            />
-          </label>
-          <label>
-            hex code:
-            <input
-              onChange={e =>
-                setColorToEdit({
-                  ...colorToEdit,
-                  code: { hex: e.target.value }
-                })
-              }
-              value={colorToEdit.code.hex}
-            />
-          </label>
-          <div className="button-row">
-            <button type="submit">save</button>
-            <button onClick={() => setEditing(false)}>cancel</button>
-          </div>
-        </form>
+        <EditColorForm saveEdit={saveEdit} setColorToEdit={setColorToEdit} colorToEdit={colorToEdit} setEditing={setEditing}/>
       )}
       <div className="spacer" />
       {/* stretch - build another form here to add a color */}
---
+++ client/src/forms/EditColorForm.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+const EditColorForm = props => {
+
+    const {saveEdit, setColorToEdit, colorToEdit,  setEditing} =props;
+
+    return (<div>
+        <form onSubmit={saveEdit}>
+          <legend>edit color</legend>
+          <label>
+            color name:
+            <input
+              onChange={e =>
+                setColorToEdit({ ...colorToEdit, color: e.target.value })
+              }
+              value={colorToEdit.color}
+            />
+          </label>
+          <label>
+            hex code:
+            <input
+              onChange={e =>
+                setColorToEdit({
+                  ...colorToEdit,
+                  code: { hex: e.target.value }
+                })
+              }
+              value={colorToEdit.code.hex}
+            />
+          </label>
+          <div className="button-row">
+            <button type="submit">save</button>
+            <button onClick={() => setEditing(false)}>cancel</button>
+          </div>
+        </form>
+    </div>)
+};
+
+export default EditColorForm;
